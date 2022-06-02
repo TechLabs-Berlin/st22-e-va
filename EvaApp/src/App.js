bdc09672c8efad3d4596c32b31216cb7f8d82dbb
SnehaGhag <102026985+SnehaGhag@users.noreply.github.com>
2022-06-02 20:15:34 +0200

    added button feature and mui samples

--- EvaApp/src/App.js
+++ EvaApp/src/App.js
@@ -1,4 +1,21 @@
 import React, { useEffect } from 'react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Chip,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  DialogTitle,
+  IconButton,
+  Link,
+  Stack,
+  TableCell,
+  TableRow,
+  Typography,
+} from '@mui/material/';
+import SnehaButton from './Component/SnehaButtom';
 
 import logo from './logo.svg';
 
@@ -26,6 +43,8 @@ function App() {
   return (
     <div className="App">
       Hello world!!
+      <Button>Register</Button>
+      <SnehaButton></SnehaButton>
     </div>
   );
 }
