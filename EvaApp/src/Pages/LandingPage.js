5caaac97ea45fdfc0a8a8ba1f9df4a9623ef89b3
SnehaGhag <102026985+SnehaGhag@users.noreply.github.com>
2022-06-05 18:38:10 +0200

    Added test content on LandingPage

--- EvaApp/src/Pages/LandingPage.js
+++ EvaApp/src/Pages/LandingPage.js
@@ -1,7 +1,28 @@
-const LandingPage = () =>{
+import {
+    Avatar,
+    Box,
+    Button,
+    Chip,
+    Dialog,
+    DialogContent,
+    DialogActions,
+    DialogTitle,
+    IconButton,
+    Link,
+    Paper,
+    Stack,
+    TableCell,
+    TableRow,
+    Typography,
+  } from '@mui/material/';
+  import SnehaButton from '../Component/SnehaButtom';
+const LandingPage = () => {
     return <div>
-        Soma
+        <Paper>This is a test Landing Page</Paper>
     </div>
 
 }
+
+
+
 export default LandingPage;
