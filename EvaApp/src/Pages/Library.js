42abd1f505aabd4a52eefbe6cd1122b650c5403c
SnehaGhag <102026985+SnehaGhag@users.noreply.github.com>
2022-07-12 20:14:58 +0200

    small edits on library page

--- EvaApp/src/Pages/Library.js
+++ EvaApp/src/Pages/Library.js
@@ -3,7 +3,6 @@ import Lib from "../Component/Lib";
 const Read = () => {
   return (
     <div>
-      <Typography>Read our blogs on Menstruation and Conception</Typography>
       <Lib />
     </div>
   );
